fde0c6cf3b50921d8fea53345f1e33ece1fe4974
Aram <Aram.v.Hovakyan@gmail.com>
2019-12-14 20:13:08 +0400

    commit

--- src/main/java/AramDemo2022/api/DoctorControllerImpl.java
+++ src/main/java/AramDemo2022/api/DoctorControllerImpl.java
@@ -1,4 +1,14 @@
 package AramDemo2022.api;
 
-public class DoctorControllerImpl {
+import AramDemo2022.common.DoctorCreatDTO;
+import AramDemo2022.common.DoctorDTO;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DoctorControllerImpl implements DoctorController {
+
+    @Override
+    public DoctorDTO create(DoctorCreatDTO doctorCreatDTO) {
+        return null;
+    }
 }
